23bdb66043fb5529c39184e3075123b45a495b14
Andrew Simmons <a.simmons@deakin.edu.au>
2020-06-06 21:25:23 +1000

    Initial Commit of Flashcard Game

---
+++ README.md
@@ -0,0 +1,55 @@
+# Flashcard Game
+
+A simple Flashcard Game written in Python to answer this question: <https://stackoverflow.com/questions/62147592/save-inputs-as-dictionary-values-forever>
+
+
+```
+$ python3 flashcard_game.py 
+(words loaded from save file)
+
+⚡⚡⚡ Play Flashcard Game ⚡⚡⚡
+
+What is the definition of azure? blue
+Almost!
+The expected answer is "bright blue"
+0.5 points awarded
+
+What is the definition of penultimate? second last
+Correct!
+The expected answer is "second last"
+1.0 points awarded
+
+What is the definition of stochastic? ?!@?  
+Wrong!
+The expected answer is "random"
+0.0 points awarded
+
+What is the definition of defenestrate? open a window
+Almost!
+The expected answer is "throw out of a window"
+0.6 points awarded
+
+Finished 🎉  Score: 2.1/4
+
+Would you like to add a new word to the game? (y/n) y
+New term: awesome
+New definition: this game
+Added!
+
+Would you like to add a new word to the game? (y/n) n
+Exiting...
+
+Saving words...
+Words saved!
+```
+
+```
+$ cat saved_words.json 
+{
+    "awesome": "this game",
+    "azure": "bright blue",
+    "defenestrate": "throw out of a window",
+    "penultimate": "second last",
+    "stochastic": "random"
+}
+```
---
+++ flashcard_game.py
@@ -0,0 +1,84 @@
+#!usr/bin/env python3
+import json
+import random
+import difflib
+
+initial_words = {
+    "azure": "bright blue",
+    "defenestrate": "throw out of a window",
+    "penultimate": "second last",
+    "stochastic": "random"
+}
+
+# Code to save the words to a file at the end of the program
+def save_words_to_file(words):
+    with open('saved_words.json', 'w') as f:
+        json.dump(words, f, indent=4, sort_keys=True)
+
+# Code to read the words from a file at the start of the next run
+def load_words_from_file():
+    with open('saved_words.json', 'r') as f:
+        words = json.load(f)
+    return words
+
+# Code to define a new custom word
+def add_new_word(words):
+    term = input("New term: ")
+    definition = input("New definition: ")
+    if term != "" and definition != "":
+        words[term] = definition
+        print("Added!\n")
+    else:
+        print("Invalid entry\n")
+
+def main():
+    # Load the words from the last run (if any)
+    try:
+        words = load_words_from_file()
+        print("(words loaded from save file)\n")
+    except OSError:
+        print("(save file does not exist yet, or could not be loaded)\n")
+        words = initial_words
+
+    # Play the game
+    print("⚡⚡⚡ Play Flashcard Game ⚡⚡⚡\n")
+    score = 0
+    terms = list(words.keys())
+    random.shuffle(terms)
+
+    try:
+        for term in terms:
+            response = input(f"What is the definition of {term}? ")
+            definition = words[term]
+            match = difflib.SequenceMatcher(a=response, b=definition)
+            similarity = match.ratio()
+            if similarity == 1:
+                print("Correct!")
+            elif similarity == 0:
+                print("Wrong!")
+            else:
+                print(f"Almost!")
+            print(f"The expected answer is \"{definition}\"")
+            points = round(similarity,1) 
+            print(f"{points} points awarded\n")
+            score += points
+
+        print(f"Finished 🎉  Score: {score}/{len(words)}\n")
+        
+        while True:
+            response = input("Would you like to add a new word to the game? (y/n) ")
+            if response.lower() != "n":
+                add_new_word(words)
+            else:
+                print("Exiting...")
+                break
+    # The finally block makes sure the words are always saved
+    # even if there is an exception/error
+    finally:
+        print("\nSaving words...")
+        # Save the custom words to a file
+        save_words_to_file(words)
+        print("Words saved!")
+
+if __name__ == "__main__":
+    main()
