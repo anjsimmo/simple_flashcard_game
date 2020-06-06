1fff4ff93e19a7bbdd5b58b5aa1631e2ed1eab89
Andrew Simmons <a.simmons@deakin.edu.au>
2020-06-06 21:50:15 +1000

    Fix formatting

--- flashcard_game.py
+++ flashcard_game.py
@@ -3,6 +3,7 @@ import json
 import random
 import difflib
 
+
 initial_words = {
     "azure": "bright blue",
     "defenestrate": "throw out of a window",
@@ -10,16 +11,19 @@ initial_words = {
     "stochastic": "random"
 }
 
+
 # Code to save the words to a file at the end of the program
 def save_words_to_file(words):
     with open('saved_words.json', 'w') as f:
         json.dump(words, f, indent=4, sort_keys=True)
+
 
 # Code to read the words from a file at the start of the next run
 def load_words_from_file():
     with open('saved_words.json', 'r') as f:
         words = json.load(f)
     return words
+
 
 # Code to define a new custom word
 def add_new_word(words):
@@ -30,6 +34,7 @@ def add_new_word(words):
         print("Added!\n")
     else:
         print("Invalid entry\n")
+
 
 def main():
     # Load the words from the last run (if any)
@@ -59,12 +64,12 @@ def main():
             else:
                 print(f"Almost!")
             print(f"The expected answer is \"{definition}\"")
-            points = round(similarity,1) 
+            points = round(similarity, 1)
             print(f"{points} points awarded\n")
             score += points
 
         print(f"Finished 🎉  Score: {score}/{len(words)}\n")
-        
+
         while True:
             response = input("Would you like to add a new word to the game? (y/n) ")
             if response.lower() != "n":
@@ -79,6 +84,7 @@ def main():
         # Save the custom words to a file
         save_words_to_file(words)
         print("Words saved!")
+
 
 if __name__ == "__main__":
     main()
